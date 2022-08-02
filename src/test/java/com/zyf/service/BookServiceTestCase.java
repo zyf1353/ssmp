69d6be3c930e602cf22ad66e768e99dae0e35191
lenovo <2431092132@qq.com>
2022-08-02 15:59:22 +0800

    update

--- src/test/java/com/zyf/service/BookServiceTestCase.java
+++ src/test/java/com/zyf/service/BookServiceTestCase.java
@@ -2,7 +2,9 @@ package com.zyf.service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class BookServiceTestCase {
 
     @Autowired
